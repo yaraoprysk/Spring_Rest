5c02020f0190a6a3ff83880611ebedb77087c476
yaraoprysk <57788210+yaraoprysk@users.noreply.github.com>
2020-03-20 10:16:32 +0200

    Add code of the third webinar

---
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -0,0 +1,42 @@
+package ua.lviv.iot.spring.first.rest.model;
+
+public class Student {
+
+	private String firstName;
+
+	private String lastName;
+
+	private Integer id;
+
+	public Student() {
+	}
+
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
